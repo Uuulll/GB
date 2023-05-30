e65c9b06091c1b58348644735099039a34bbdb04
Ulllll <nooryes1@gmail.com>
2023-05-30 20:08:10 +0300

    gfhgfhf

---
+++ GB/GB/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GB
+//
+//  Created by Анастасия Рябова on 30.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
